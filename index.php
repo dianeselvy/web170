859d82f9b0712e4622a4b2d4d93cb7d3254f01eb
dianeselvy <dianeselvy@hotmail.com>
2016-04-16 21:01:08 -0700

    Test?
    No idea what I'm doing! This should be my staging area code

---
+++ index.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+<title>Diane Selvy | WEB170 Staging Area</title>
+<div id="header">
+<h1>Diane Selvy - WEB170</h1>
+<h2>Staging Area</h2>
+</div>
+</head>
+
+<style>
+#header {
+	text-align:center;
+	font-family:courier new;
+}
+#header h1{
+	padding-top:50px;
+}
+#content {
+	font-family:courier new;
+	width:840px;
+	margin:0px auto;
+	padding:25px;
+	background:#C39BD3;
+	border-radius:25px;
+	text-decoration:none;
+}
+#content p{
+	margin-left:25px;
+}
+#content a{
+	text-decoration:none;
+}
+footer p{
+	text-align:center;
+}
+#temps {
+	margin-left:32%;
+}
+#protosite {
+	margin-left:56%;
+	margin-top:-115px;
+}
+</style>
+
+
+<body>
+<div id="content">
+<div id="temps"><h3>Templates</h3>
+<p><a href="">View</a></p>
+<p><a href="">Code</a></p>
+</div>
+<div id="protosite"><h3>Protosite</h3>
+<p><a href="">View</a></p>
+<p><a href="">Code</a></p>
+</div>
+</div>
+</body>
+<footer>
+<p>&copy; Diane Selvy | All Rights Reserved 2016</p>
+</footer>
+</html>
