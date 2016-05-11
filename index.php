b3400ce950e3decfe48ab2e21ce037de0b8244d3
dianeselvy <dianeselvy@hotmail.com>
2016-05-11 09:29:07 -0700

    Updated files. Messed up before
    Woohoo

--- index.php
+++ index.php
@@ -1,61 +1,20 @@
-<html>
-<head>
-<title>Diane Selvy | WEB170 Staging Area</title>
-<div id="header">
-<h1>Diane Selvy - WEB170</h1>
-<h2>Staging Area</h2>
-</div>
-</head>
-
-<style>
-#header {
-	text-align:center;
-	font-family:courier new;
-}
-#header h1{
-	padding-top:50px;
-}
-#content {
-	font-family:courier new;
-	width:840px;
-	margin:0px auto;
-	padding:25px;
-	background:#C39BD3;
-	border-radius:25px;
-	text-decoration:none;
-}
-#content p{
-	margin-left:25px;
-}
-#content a{
-	text-decoration:none;
-}
-footer p{
-	text-align:center;
-}
-#temps {
-	margin-left:32%;
-}
-#protosite {
-	margin-left:56%;
-	margin-top:-115px;
-}
-</style>
+<?php get_header(); ?>
 
 
-<body>
-<div id="content">
-<div id="temps"><h3>Templates</h3>
-<p><a href="">View</a></p>
-<p><a href="">Code</a></p>
+
+<!--WP loop that displays content-->
+<body <?php body_class(); ?>>
+  <div id="content">
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); //start the loop ?>
+    <h2><a href="<?php the_permalink(); //link to the page or posting ?>"><?php the_title(); //get page or posting title ?></a></h2>
+<?php the_content(''); //get page or posting or written content ?>
+<?php endwhile; endif; //end the loop ?>
 </div>
-<div id="protosite"><h3>Protosite</h3>
-<p><a href="">View</a></p>
-<p><a href="">Code</a></p>
-</div>
-</div>
+
+    
+
 </body>
-<footer>
-<p>&copy; Diane Selvy | All Rights Reserved 2016</p>
-</footer>
-</html>
+
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -3,12 +3,12 @@
 
 
 <!--WP loop that displays content-->
-<body <?php body_class(); ?>>
   <div id="content">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); //start the loop ?>
-    <h2><a href="<?php the_permalink(); //link to the page or posting ?>"><?php the_title(); //get page or posting title ?></a></h2>
+<h2><a href="<?php the_permalink(); //link to the page or posting ?>"><?php the_title(); //get page or posting title ?></a></h2>
 <?php the_content(''); //get page or posting or written content ?>
 <?php endwhile; endif; //end the loop ?>
+      <p>single.php</p>
 </div>
 
     
