be23f2cafdbb3d0284bd8a98617e88d0437d1a6b
dianeselvy <dianeselvy@hotmail.com>
2016-05-16 16:27:57 -0700

    Removes comments that were copied from Mike's code
    Sorry mike :(

--- page.php
+++ page.php
@@ -1,11 +1,10 @@
 <?php get_header(); ?>
 
-<!--WP loop that displays content-->
   <div id="content">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); //start the loop ?>
-<h2><?php the_title(); //get page or posting title ?></h2>
-<?php the_content(''); //get page or posting or written content ?>
-<?php endwhile; endif; //end the loop ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<h2><?php the_title(); ?></h2>
+<?php the_content(''); ?>
+<?php endwhile; endif; ?>
       <p>page.php</p>
 </div>
 
