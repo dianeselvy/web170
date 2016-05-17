1bbc39654a9875278f2409e557780ecc72ae2b83
dianeselvy <dianeselvy@hotmail.com>
2016-05-16 17:58:47 -0700

    Removed more comments

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body <?php body_class(); ?>>
   <div id="content">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <h2><a href="<?php the_permalink(); //link to the page or posting ?>"><?php the_title(); ?></a></h2>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 <?php the_content(''); ?>
 <?php endwhile; endif; ?>
 </div>
