c19fbf51bd539be6bf5b08e3235c85ddcfe1696a
dianeselvy <dianeselvy@hotmail.com>
2016-05-16 19:00:16 -0700

    I dont really remember what this does
    I know it's not super significant though

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <div id="content">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <small>Posted on <?php the_time(F j, Y); ?> from <?php the_posting(); ?></small>
 <?php the_content(''); ?>
 <?php endwhile; endif; ?>
 </div>
