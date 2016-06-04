7494eea402c85ee3f921d150539312719478e281
dianeselvy <dianeselvy@hotmail.com>
2016-06-03 18:39:32 -0700

    Featured Images
    09) Creating a Custom Posts Feed with Featured Images for Your Site

--- page.php
+++ page.php
@@ -1,12 +1,13 @@
 <?php get_header(); ?>
 
+<!--WP loop -->
   <div id="content">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<h2><?php the_title(); ?></h2>
-<?php the_content(''); ?>
-<?php endwhile; endif; ?>
-      <p>page.php</p>
-</div>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<h2><?php the_title(); ?></h2>
+	<?php the_content(''); ?>
+	<?php endwhile; endif; ?>
+	<p>page.php</p>
+  </div>
 
     
 
